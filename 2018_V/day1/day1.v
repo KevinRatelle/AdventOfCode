a34e337f4856ff890dd3949d7e8425cc54f5aa50
KevinRatelle <kevinratelle@hotmail.com>
2024-04-12 13:34:52 -0400

    [2018] [V] day 1 part b

--- 2018_V/day1/day1.v
+++ 2018_V/day1/day1.v
@@ -1,5 +1,7 @@
 module day1
 
+import arrays
+
 pub fn compute_result(lines []string) int
 {
     mut total := int(0)
@@ -13,10 +15,53 @@ pub fn compute_result(lines []string) int
         }
 
         value := line[1..line.len].u32()
-
-        println('${line} : ${value}')
-
         total += (mult * value)
+    }
+
+    return total
+}
+
+pub fn compute_result_part_b(lines []string) int
+{
+    mut arr := []int{}
+    mut total := int(0)
+
+    for
+    {
+        mut found := false
+
+        for line in lines
+        {
+            arr = arrays.concat(arr, total)
+
+            mut mult := int(1)
+            first_char := line[0]
+            if first_char == `-`
+            {
+                mult = -1
+            }
+
+            value := line[1..line.len].int()
+            total += (mult * value)
+
+            for v in arr
+            {
+                if total == v
+                {
+                    found = true
+                }
+            }
+
+            if found
+            {
+                break
+            }
+        }
+
+        if found
+        {
+            break
+        }
     }
 
     return total
--- 2018_V/hello.v
+++ 2018_V/hello.v
@@ -5,7 +5,7 @@ import day1
 fn main()
 {
     lines := os.read_lines('day1.txt')!
-    total := day1.compute_result(lines)
+    total := day1.compute_result_part_b(lines)
 
 
     println('Hello, ${total} !')
